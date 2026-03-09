d2ea019ffb051a1d3b4c580fb53ee94bd93c87e1
frankprimo <frankprimo@yahoo.com.br>
2026-03-09 20:53:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,40 +6,35 @@ app = Flask(__name__)
 def home():
 
     produtos = [
-
         {
-            "nome": "PC Completo Intel i3 + Monitor 20",
+            "nome": "PC Completo Intel i3",
             "imagem": "https://m.media-amazon.com/images/I/71T1e2NG20L._AC_SY355_.jpg",
             "preco": "R$ 1.199,00",
-            "link": "https://a.co/seulink1"
+            "link": "https://amazon.com"
         },
-
         {
             "nome": "Notebook Lenovo IdeaPad",
             "imagem": "https://m.media-amazon.com/images/I/61Dw5Z8LzJL._AC_SL1000_.jpg",
             "preco": "R$ 2.199,00",
-            "link": "https://a.co/seulink2"
+            "link": "https://amazon.com"
         },
-
         {
-            "nome": "Monitor LG 24 Polegadas",
+            "nome": "Monitor LG 24",
             "imagem": "https://m.media-amazon.com/images/I/71vFKBpKakL._AC_SL1500_.jpg",
             "preco": "R$ 899,00",
-            "link": "https://a.co/seulink3"
+            "link": "https://amazon.com"
         },
-         {
+        {
             "nome": "Mouse Gamer Logitech",
             "imagem": "https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg",
             "preco": "R$ 149,00",
-            "link": "https://a.co/seulink4"
-        },
-
+            "link": "https://amazon.com"
+        }
     ]
 
     html_produtos = ""
 
     for p in produtos:
-
         html_produtos += f"""
         <div style="background:white;padding:25px;margin:20px auto;
         border-radius:15px;max-width:450px;
@@ -47,20 +42,15 @@ def home():
 
         <h2 style="text-align:center">{p["nome"]}</h2>
 
-        <img src="{p["imagem"]}" 
-        style="width:100%;max-height:260px;object-fit:contain">
+        <img src="{p["imagem"]}" style="width:100%;max-height:260px;object-fit:contain">
 
-        <h3 style="color:green;text-align:center">
-        {p["preco"]}
-        </h3>
+        <h3 style="color:green;text-align:center">{p["preco"]}</h3>
 
         <a href="{p["link"]}" target="_blank">
         <button style="background:#ff9900;border:none;
         padding:18px;width:100%;font-size:18px;
         border-radius:8px;font-weight:bold">
-
         VER NA AMAZON
-
         </button>
         </a>
 
@@ -84,8 +74,5 @@ def home():
     </html>
     """
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
