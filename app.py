d69856eea12af5eb9f959e9444f31a0c20e22f7c
frankprimo <frankprimo@yahoo.com.br>
2026-03-14 15:01:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     "categoria":"Computador",
     "avaliacao":"⭐⭐⭐⭐☆",
     "imagem":"https://m.media-amazon.com/images/I/71T1e2NG20L._AC_SY355_.jpg",
-    "preco":"R$ 1.065,00",
+    "preco":"Só na AMAZON",
     "processador":"Intel Core i3",
     "memoria":"8GB RAM",
     "armazenamento":"SSD 240GB",
@@ -28,7 +28,7 @@ def home():
     "categoria":"Notebook",
     "avaliacao":"⭐⭐⭐⭐⭐",
     "imagem":"https://m.media-amazon.com/images/I/71uv+p19nTL._AC_SY355_.jpg",
-    "preco":"R$ 2.789,00",
+    "preco":"Só na AMAZON",
     "processador":"Intel Core i5",
     "memoria":"8GB RAM",
     "armazenamento":"SSD 256GB",
@@ -42,7 +42,7 @@ def home():
     "categoria":"Monitor",
     "avaliacao":"⭐⭐⭐⭐☆",
     "imagem":"https://m.media-amazon.com/images/I/61hxA0+MEWL._AC_SY355_.jpg",
-    "preco":"R$ 500,00",
+    "preco":"Só na AMAZON",
     "tela":"24 polegadas",
     "resolucao":"1920x1080",
     "taxa":"75Hz",
@@ -55,12 +55,84 @@ def home():
     "categoria":"Periférico",
     "avaliacao":"⭐⭐⭐⭐⭐",
     "imagem":"https://m.media-amazon.com/images/I/51sg9BLSMTL._AC_SY355_.jpg",
-    "preco":"R$ 169,00",
+    "preco":"Só na AMAZON",
     "dpi":"12000 DPI",
     "botoes":"6 programáveis",
     "bateria":"até 250 horas",
     "conexao":"LIGHTSPEED sem fio",
     "link":"https://amzn.to/4uidzx0"
+    },
+
+    {
+    "nome":"Teclado Gamer Redragon Kumara",
+    "categoria":"Periférico",
+    "avaliacao":"⭐⭐⭐⭐⭐",
+    "imagem":"https://m.media-amazon.com/images/I/61Z9sR8i1lL._AC_SY355_.jpg",
+    "preco":"Só na AMAZON",
+    "switch":"Outemu Blue",
+    "iluminacao":"LED Vermelho",
+    "conexao":"USB",
+    "link":"https://amzn.to/exemplo1"
+    },
+
+    {
+    "nome":"Headset Gamer HyperX Cloud Stinger",
+    "categoria":"Áudio",
+    "avaliacao":"⭐⭐⭐⭐⭐",
+    "imagem":"https://m.media-amazon.com/images/I/61iYJk3E6sL._AC_SY355_.jpg",
+    "preco":"Só na AMAZON",
+    "driver":"50mm",
+    "microfone":"Com cancelamento",
+    "conexao":"P2",
+    "link":"https://amzn.to/exemplo2"
+    },
+
+    {
+    "nome":"SSD Kingston 480GB",
+    "categoria":"Armazenamento",
+    "avaliacao":"⭐⭐⭐⭐⭐",
+    "imagem":"https://m.media-amazon.com/images/I/61nvsBnH7CL._AC_SX450_.jpg",
+    "preco":"Só na AMAZON",
+    "capacidade":"480GB",
+    "velocidade":"500MB/s",
+    "interface":"SATA",
+    "link":"https://amzn.to/3NAEXWE"
+    },
+
+    {
+    "nome":"Memória RAM 16GB Corsair",
+    "categoria":"Memória",
+    "avaliacao":"⭐⭐⭐⭐⭐",
+    "imagem":"https://m.media-amazon.com/images/I/61a7pZ4K0XL._AC_SY355_.jpg",
+    "preco":"Só na AMAZON",
+    "capacidade":"16GB",
+    "frequencia":"3200MHz",
+    "tipo":"DDR4",
+    "link":"https://amzn.to/4sOo4qA"
+    },
+
+    {
+    "nome":"Webcam Logitech C920",
+    "categoria":"Acessório",
+    "avaliacao":"⭐⭐⭐⭐⭐",
+    "imagem":"https://m.media-amazon.com/images/I/61-6uAf8soL._AC_SY355_.jpg",
+    "preco":"Só na AMAZON",
+    "resolucao":"Full HD 1080p",
+    "microfone":"Duplo integrado",
+    "conexao":"USB",
+    "link":"https://amzn.to/4lzwXls"
+    },
+
+    {
+    "nome":"Cadeira Gamer ThunderX3",
+    "categoria":"Gamer",
+    "avaliacao":"⭐⭐⭐⭐☆",
+    "imagem":"https://m.media-amazon.com/images/I/71VqjPlOJAL._AC_SY355_.jpg",
+    "preco":"Só na AMAZON",
+    "material":"Couro sintético",
+    "ajuste":"Altura e inclinação",
+    "suporte":"até 120kg",
+    "link":"https://amzn.to/4su2rw1"
     }
 
     ]
@@ -112,7 +184,6 @@ def home():
 <head>
 
 <meta charset="UTF-8">
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Ofertas Tecnológicas</title>
